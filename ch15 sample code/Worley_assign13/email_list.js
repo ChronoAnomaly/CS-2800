f2b249be726f1904100541965e0d4a65133313c3
ChronoAnomaly <chronoanomaly@gmail.com>
2015-12-01 18:09:23 -0500

    cleaned

--- ch15 sample code/Worley_assign13/email_list.js
+++ ch15 sample code/Worley_assign13/email_list.js
@@ -34,35 +34,6 @@ $(document).ready(function() {
 			} else {
 				$("#password2").next().text("");
 			}
-
-
-			// // validate the first email address
-			// if (emailAddress1 == "") { 
-			// 	$("#email_address1").next().text("This field is required.");
-			// 	isValid = false;
-			// } else {
-			// 	$("#email_address1").next().text("");
-			// } 
-			
-			// // validate the second email address
-			// if (emailAddress2 == "") { 
-			// 	$("#email_address2").next().text("This field is required.");
-			// 	isValid = false; 
-			// } else if (emailAddress1 !== emailAddress2) { 
-			// 	$("#email_address2").next().text("This entry must equal first entry.");
-			// 	isValid = false;
-			// } else {
-			// 	$("#email_address2").next().text("");
-			// }
-			
-			// // validate the first name entry  
-			// if ($("#first_name").val() == "") {
-			// 	$("#first_name").next().text("This field is required.");
-			// 	isValid = false;
-			// } 
-			// else {
-			// 	$("#first_name").next().text("");
-			// }
 			
 			// submit the form if all entries are valid
 			if (isValid) {
